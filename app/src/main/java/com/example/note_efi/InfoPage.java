104bc473c275bfd2ea1c4ca9c7bec8c7033ea0f2
Michal <mmalek3@hawk.iit.edu>
2022-02-21 18:06:07 -0600

    Design updates

--- app/src/main/java/com/example/note_efi/InfoPage.java
+++ app/src/main/java/com/example/note_efi/InfoPage.java
@@ -5,14 +5,13 @@ import android.view.Menu;
 import android.view.MenuInflater;
 
 public class InfoPage extends MainActivity{
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_page);
-    }
+    }*/
 
 
-    @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.note_menu, menu);

c38a3ff3b474130591da13e8e5f030db2f410300
Michal <malek.michal.j@gmail.com>
2022-02-28 01:21:52 -0600

    added background image and new activity page push#2

--- app/src/main/java/com/example/note_efi/MakeNote.java
+++ app/src/main/java/com/example/note_efi/MakeNote.java
@@ -3,6 +3,7 @@ package com.example.note_efi;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MakeNote extends MainActivity{
     @Override
@@ -15,5 +16,12 @@ public class MakeNote extends MainActivity{
         MenuInflater inflater2 = getMenuInflater();
         inflater2.inflate(R.menu.note_menu, menu);
         return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.save) {
+            finish();
+        }
+            return true;
     }
 }
